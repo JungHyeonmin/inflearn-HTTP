5c8b5b81d5b4d7ffa2362b2483d8f9d224a0439b
JungHyeonmin <2ee42a45@gmail.com>
2025-01-06 17:08:51 +0900

    Feat: 프로젝트 생성

---
+++ inflearn-http/src/main/java/com/jhm/inflearnhttp/InflearnHttpApplication.java
@@ -0,0 +1,13 @@
+package com.jhm.inflearnhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnHttpApplication.class, args);
+    }
+
+}
---
+++ inflearn-http/src/test/java/com/jhm/inflearnhttp/InflearnHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhm.inflearnhttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnHttpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
